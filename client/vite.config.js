dd0fe6ad1bb5bcac0bbc0734fd449c1883287d4b
michellecong <cong.m@northeastern.edu>
2025-03-26 01:43:00 -0700

    adjust config

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,10 +6,15 @@ export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
+    base: "/", // 添加基础路径配置
+
     environment: "jsdom",
     setupFiles: "./src/setupTests.js",
   },
   build: {
     outDir: "build", // Change output directory from 'dist' to 'build'
+  },
+  server: {
+    historyApiFallback: true, // 添加历史 API 回退支持
   },
 });
