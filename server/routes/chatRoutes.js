b972aad54324e79a089f00bde2512c940769c211
Shurui-Liu <shurui.liu.710@gmail.com>
2025-04-04 18:32:02 -0700

    refactor: remove commented-out validation code from chatRoutes

--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -2,9 +2,6 @@ const express = require("express");
 const router = express.Router();
 const { chatMessage } = require("../controllers/chatController");
 
-// Optional: you can remove validation while testing
-// const { check, validationResult } = require("express-validator");
-
 router.post("/", chatMessage);
 
 module.exports = router;
