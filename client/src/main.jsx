177cdbbe1e410ffaccbf4d635a299ff6cbe17e6e
Shurui-Liu <shurui.liu.710@gmail.com>
2025-04-11 19:16:26 -0700

    Add Auth0 authentication and Express dependencies.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// main.jsx
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Auth0Provider } from "@auth0/auth0-react";
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+const domain = "dev-mbnzpg6jdhzwez57.us.auth0.com";
+const clientId = "aUFLFb8hIWukzOXGOy6kbKWO8JAP9Pxz";
+const audience = "https://example_end_point/"; // from Auth0 API settings
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+        audience: audience,
+        scope: "openid profile email",
+      }}
+    >
+      <App />
+    </Auth0Provider>
+  </StrictMode>
+);
